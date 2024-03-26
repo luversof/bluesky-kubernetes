bce2eeed6cd55d260f8cf0aca3499e1b74d993e1
luversof <luversof@hanmail.net>
2024-03-26 17:42:24 +0900

    sonarqube, jenkins 제거 (docker, 실행 bat 파일 제외)

--- jenkins/jenkins.bat
+++ jenkins/jenkins.bat
@@ -1,3 +1,3 @@
 call docker pull jenkins/jenkins:jdk21
-call docker build -f jenkins-Dockerfile --tag luversof/jenkins .
+# call docker build -f jenkins-Dockerfile --tag luversof/jenkins .
 call kubectl delete pods -l app=jenkins
