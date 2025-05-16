a2f2e6cc47eacc42707ac1a5d27e6de04d5ed074
luversof <luversof@hanmail.net>
2025-05-16 20:45:11 +0900

    Update Argo CD manifest URL to version v3.0.1
    - Bump Argo CD version from v2.13.3 to v3.0.1
    - Modify `argocd.bat` to use the updated manifest URL
    - Ensure compatibility with the latest Argo CD release
    - Retain existing local `kubectl` apply command
    - No other changes made to the script
    - Fix missing newline at end of file

--- argocd/argocd.bat
+++ argocd/argocd.bat
@@ -1,2 +1,2 @@
-call kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.13.3/manifests/install.yaml
+call kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v3.0.1/manifests/install.yaml
 call kubectl -n argocd apply -f .
