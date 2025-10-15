ef9817eab1a8efb54b27df0aa4386591c88a20e2
luversof <luversof@hanmail.net>
2025-10-15 16:17:15 +0900

    Add helm repo update command to Hoppscotch installation script

--- hoppscotch/hoppscotch-install.bat
+++ hoppscotch/hoppscotch-install.bat
@@ -1,2 +1,3 @@
+call helm repo update
 call helm repo add hoppscotch https://hoppscotch.github.io/helm-charts
 call helm install -f values.yaml hoppscotch hoppscotch/hoppscotch-community --namespace opensource
