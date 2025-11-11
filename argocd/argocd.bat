b9d280e8ac3fec0ecdf542a7cac9e8e93622c717
luversof <luversof@hanmail.net>
2025-11-12 06:41:49 +0900

    Add ConfigMap patch for Argo CD and update deployment commands

--- argocd/argocd.bat
+++ argocd/argocd.bat
@@ -1,2 +1,3 @@
 call kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v3.1.9/manifests/install.yaml
-call kubectl -n argocd apply -f .
+call kubectl patch -n argocd configmap argocd-cm --type merge --patch-file=argocd-cm-patch.yaml
+call kubectl -n argocd apply -f argocd-cmd-params-cm.yaml,argocd-ing.yaml
