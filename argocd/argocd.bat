2c2383459f064a328b91969d32ffccb0b9b9c09d
luversof <luversof@hanmail.net>
2026-02-04 07:55:17 +0900

    Update Argo CD manifest URL to version v3.2.6

--- argocd/argocd.bat
+++ argocd/argocd.bat
@@ -1,3 +1,3 @@
-call kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v3.2.3/manifests/install.yaml
+call kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v3.2.6/manifests/install.yaml
 call kubectl patch -n argocd configmap argocd-cm --type merge --patch-file=argocd-cm-patch.yaml
 call kubectl -n argocd apply -f argocd-cmd-params-cm.yaml,argocd-ing.yaml
